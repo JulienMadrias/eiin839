f89e4587b260fac137921bcb548b00eaa0f853d1
JulienMadrias <julien.madrias@etu.univ-cotedazur.fr>
2021-03-17 15:06:17 +0100

    enregistrement TD

---
+++ TD2/FirstSoapCalc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSoapCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.CalculatorSoapClient calc = new Calculator.CalculatorSoapClient();
+            Console.WriteLine(calc.Add(1, 3));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ webFront/FirstSOAP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.CalculatorSoapClient calc = new Calculator.CalculatorSoapClient();
+            Console.WriteLine(calc.AddAsync(1,3));
+        }
+    }
+}
